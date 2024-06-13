db6b6b0f04be6dc9545d0340d60670dfd35d55ce
Xander <alexanderdeisseroth@gmail.com>
2024-06-12 17:29:57 -0700

    removed unneccessary files

--- stage.js
+++
@@ -1,6 +0,0 @@
-const stage = {
-    blue: require("./assets/field_drawings/stage-blue.png"),
-    red: require("./assets/field_drawings/stage-red.png"),
-};
-
-export default stage;
--- store/store.js
+++
@@ -1,5 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-
-import rootReducer from './reducer';
-
-export default store = configureStore({ reducer: rootReducer });
